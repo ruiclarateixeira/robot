3ba70136c4f2c7e734f7d067f5148c1597661066
Rui Teixeira <rui.clarateixeira@student.manchester.ac.uk>
2017-01-29 21:41:08 +0000

    Remove unnecessary pin

--- lib/wheels.py
+++ lib/wheels.py
@@ -1,14 +1,10 @@
 import RPi.GPIO as GPIO
 
-motor_left_on_pin = 22
 motor_left_forward_pin = 18
 motor_left_backward_pin = 16
 
 def init():
     GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(motor_left_on_pin, GPIO.OUT)
-    GPIO.output(motor_left_on_pin, GPIO.LOW)
-
     GPIO.setup(motor_left_forward_pin, GPIO.OUT)
     GPIO.setup(motor_left_backward_pin, GPIO.OUT)
 
@@ -16,7 +12,7 @@ def move_forward():
     print "Moving forward"
     GPIO.output(motor_left_forward_pin, GPIO.HIGH)
     GPIO.output(motor_left_backward_pin, GPIO.LOW)
-    GPIO.output(motor_left_on_pin, GPIO.HIGH)
 
 def stop():
     GPIO.output(motor_left_on_pin, GPIO.LOW)
+    GPIO.output(motor_left_forward_pin, GPIO.HIGH)
