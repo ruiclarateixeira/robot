995734b5f9a916d73458f276833d6b239b075f8e
Rui Teixeira <rui.clarateixeira@student.manchester.ac.uk>
2017-05-21 18:25:29 +0100

    TCP IP Adress can be an argument

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import signal
 import SocketServer
 import os
 from time import sleep
-from sys import platform
+from sys import platform, argv
 from tcphandler import MyTCPHandler
 from threading import Thread
 import wheels
@@ -13,7 +13,11 @@ import wheels
 wheels.init()
 
 def start_tcp_listener():
-    TCP_HOST, TCP_PORT = "localhost", 5001
+    if len(argv) > 1:
+        TCP_HOST, TCP_PORT = argv[1], 5001
+    else:
+        TCP_HOST, TCP_PORT = "localhost", 5001
+        
     print "Starting TCP socket on {}:{}".format(TCP_HOST, TCP_PORT)
     server = SocketServer.TCPServer((TCP_HOST, TCP_PORT), MyTCPHandler)
     server.serve_forever()
