b2cd7bebce5c842922e3c3c1a75976fd63ed522b
Rui Teixeira <rui.clarateixeira@student.manchester.ac.uk>
2017-05-28 18:51:24 +0100

    Make UI mobile friendly

--- UI/scripts/index.js
+++ UI/scripts/index.js
@@ -1,11 +1,15 @@
 var connection = null;
 
+function updateConnectionStatus(newStatus) {
+   $("#connection-status").text(newStatus);
+}
+
 function connect() {
   var ip = $('#ip-add').val();
   connection = new WebSocket("ws://" + ip + ":5001");
 
   connection.onopen = function () {
-    connection.send('Ping');
+      updateConnectionStatus("Connected!");
   };
 
   connection.onerror = function (error) {
@@ -38,10 +42,10 @@ function stop() {
 }
 
 function init() {
-  $("#up").mousedown(moveForward).mouseup(stop);
-  $("#down").mousedown(moveBackward).mouseup(stop);
-  $("#left").mousedown(turnLeft).mouseup(stop);
-  $("#right").mousedown(turn_right).mouseup(stop);
+  $("#up").bind("touchstart", moveForward).bind("touchend", moveForward).mousedown(moveForward).mouseup(stop);
+  $("#down").bind("touchstart", moveBackward).bind("touchend", moveBackward).mousedown(moveBackward).mouseup(stop);
+  $("#left").bind("touchstart", turnLeft).bind("touchend", turnLeft).mousedown(turnLeft).mouseup(stop);
+  $("#right").bind("touchstart", turnRight).bind("touchend", turnRight).mousedown(turnRight).mouseup(stop);
 }
 
 $(init);
